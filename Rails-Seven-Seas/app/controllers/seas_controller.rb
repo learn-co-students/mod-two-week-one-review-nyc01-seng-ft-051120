f0b56ef1b0d5b633c31c6735d8006baf370f3a78
github username <github email address>
2020-06-05 13:09:16 -0400

    only missing delete

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,37 @@
 class SeasController < ApplicationController
   #define your controller actions here
+ def index 
+  @seas = Sea.all
+ end
+ def show
+  @sea = Sea.find(params[:id])
+ end
+
+ def new
+  @sea = Sea.new
+ end
+
+def create
+  @sea = Sea.new(sea_params)
+  @sea.save
+  redirect_to sea_path(@sea)
+end
+def edit
+  @sea = Sea.find(params[:id])
+end
+
+def update
+@sea= Sea.find(params[:id])
+@sea.update(sea_params)
+redirect_to sea_path(@sea)
+end
+
+
+def delete
+@sea = Sea.find(params[:id])
+@sea.destroy
+redirect to seas_url
+end
 
 
   private
@@ -9,5 +41,4 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
-
 end
