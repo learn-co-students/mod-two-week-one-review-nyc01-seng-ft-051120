f9f26eee1c9aafeb93a2b95bc51e64f3ec42619e
emotionalproof <chaserabenn@gmail.com>
2020-06-05 12:59:04 -0400

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get "/seas", to:'seas#index'
+  get "/", to: 'seas#welcome'
+  get "seas/new", to: 'seas#new'
+  get 'seas/:id', to: 'seas#show'
+  post '/seas'
+  # resources :seas
+  # get	'/seas/:id/edit'	/seas/edit
+  # resources :seas, only: :show
 end
+
